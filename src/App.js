c8b37b79a0c255e1ee990310754851d7fb39bfc0
KUMMARI KESHANNA <keshava2627@keshannakummarik@gmail.com>
2023-12-10 20:03:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/LoginPage/Login";
+import OrderList from "./pages/Order/OrderList";
+import Products from "./pages/Products/Products";
+import { User } from "./pages/Users/User";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/order" element={<OrderList />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
